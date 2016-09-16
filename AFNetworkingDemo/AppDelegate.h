9df6af1061b8afedc023de583ac148b2a2d1744e
pkguptahbti22 <pramod@micrasystems.com>
2016-09-16 11:22:08 +0530

    Initial Commit

---
+++ AFNetworkingDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AFNetworkingDemo
+//
+//  Created by micra systems on 16/09/16.
+//  Copyright © 2016 micra systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AFNetworkingDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AFNetworkingDemo
+//
+//  Created by micra systems on 16/09/16.
+//  Copyright © 2016 micra systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
